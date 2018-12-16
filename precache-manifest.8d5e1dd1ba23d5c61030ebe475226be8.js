ec879b837090684843a4f568919eb14c75cacb01
datfc97pro <datfc97pro@gmail.com>
2018-12-16 11:00:47 +0700

    Updates

---
+++ precache-manifest.8d5e1dd1ba23d5c61030ebe475226be8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/deploy-react/static/media/showcase.4b31330b.jpg"
+  },
+  {
+    "revision": "a72cbe425345309916a7",
+    "url": "/deploy-react/static/js/runtime~main.a72cbe42.js"
+  },
+  {
+    "revision": "33c4cb0154588be486b3",
+    "url": "/deploy-react/static/js/main.33c4cb01.chunk.js"
+  },
+  {
+    "revision": "cb393e08679d5c271574",
+    "url": "/deploy-react/static/js/1.cb393e08.chunk.js"
+  },
+  {
+    "revision": "33c4cb0154588be486b3",
+    "url": "/deploy-react/static/css/main.f5600d37.chunk.css"
+  },
+  {
+    "revision": "431aa3122360b126e73adcd15aba56bf",
+    "url": "/deploy-react/index.html"
+  }
+];
